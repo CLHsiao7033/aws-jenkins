c29f16c7be6c7e8cfaeb350e0dc80d86e9ce6e8e
CLHsiao7033 <110364009+CLHsiao7033@users.noreply.github.com>
2023-10-17 10:31:47 +0800

    Update main.py

--- k8s-nginx/app/main.py
+++ k8s-nginx/app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "World 4444"}
+    return {"Hello": "World 777777"}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
